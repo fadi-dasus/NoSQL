848521c63b3ab6529c001e84d7b816f185593bd2
fadi-dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-04-09 11:01:23 +0200

    init

--- script/functions.js
+++ script/functions.js
@@ -1,37 +1,4 @@
-1.	Sell a book to a customer.
-8.	Sell 3 copies of one book and 2 of another in a single order
-
-//successful scenario
-
-var order = {
-    _id : 18,
-    date : "2020-05-11",
-    book : [ 
-        {
-            price : NumberDecimal("10.0"),
-            ISBN : "a1",
-            qty : NumberInt(3)
-        }, 
-        {
-            price : NumberDecimal("100.0"),
-            ISBN : "a2",
-            qty : NumberInt(2)
-        }
-    ],
-    state : "initial"
-}
-
-
-           
-db.order.insert(order)
-
-
-var result = db.order.update({_id:order._id,state:'initial'},
-{$set:{state:'pending'},$currentDate:{lastModified:true}})
-
-result.nModified
-
-
+db.loadServerScripts();
 
 
 db.system.js.save(
@@ -47,6 +14,10 @@ db.system.js.save(
 
    }
 );
+
+
+
+
 
 db.system.js.save(
    {
@@ -162,66 +133,6 @@ db.system.js.save(
 
 
 
-db.loadServerScripts();
-   
-   
-   
-applyPendingOrder(order)
-   
-var result = db.order.update({_id:order._id,state:'pending'},
-{$set:{state:'applied'},$currentDate:{lastModified:true}})   
-
-result.nModified
-
-
-var result = removePendingOrder(order)
-result
-  
-
-var result = db.order.update({_id:order._id,state:'applied'},
-{$set:{state:'done'},$currentDate:{lastModified:true}})   
-
-
-
-_______
-
-//fail scenario 
-#not enough copies (the order must fail and then we will rollback)
-
-var initialOrder = {
-    _id : 21,
-    date : "2020-05-11",
-    book : [ 
-        {
-            price : NumberDecimal("10.0"),
-            ISBN : "a1",
-            qty : NumberInt(1000)
-        }, 
-        {
-            price : NumberDecimal("100.0"),
-            ISBN : "a2",
-            qty : NumberInt(1)
-        }
-    ],
-    state : "initial"
-}
-
-db.order.remove({_id:21})
-
-db.order.insert(initialOrder)
-
-var result = db.order.update({_id:initialOrder._id,state:'initial'},
-{$set:{state:'pending'},$currentDate:{lastModified:true}})
-
-result.nModified
-
-var myOrder = db.order.find({_id:initialOrder._id})
-
-
-var result = applyPendingOrder(initialOrder)
- 
-result // false
-
 
 db.system.js.save(
    {
@@ -243,23 +154,10 @@ db.system.js.save(
    }
 );
 
-db.loadServerScripts();
-
-
-var failedItems = getFailedItems(initialOrder)
-
-failedItems
-   
-
-var result = db.order.update({_id:initialOrder._id,state:'pending'},
-{$set:{state:'cancelling'},$currentDate:{lastModified:true}})
-
-result.nModified   
 
 
 
-
-   
+ 
 db.system.js.save(
    {
        
@@ -345,30 +243,8 @@ db.system.js.save(
 
    }
 );
-   
-   
-db.loadServerScripts();
 
 
-var success = revertCancellingOrder(initialOrder,failedItems)
-
-success
-  
-   
-______________
-
-2.	Change the address of a customer.
-
-db.customer.update({_id:1},{$set:{'address.street':'newStreetName'}})
-_______________________________
-
-3.	 Add an existing author to a book.
-var author = db.author.find({_id:4}).next()._id
-db.book.update({ISBN:'a1'},{$pull:{author:author}})
-_______________________________
-
-4.	Retire the "Space Opera" category and assign all books from that category to the parent category. Don't assume you know the id of the parent category.
-7.	Retire the "Fantasy and Science Fiction" category and just use either "Fantasy" or "Science Fiction"
 
 db.system.js.save(
    {      
@@ -417,25 +293,4 @@ db.system.js.save(
 
 }
    }
-);
-   
-var category = db.category.find({name:'Space Opera'}).next()
-var books = db.book.find({category:{$elemMatch:{$eq:category}}}).toArray() 
-books 
-db.loadServerScripts();
-
-
-var result = updateBookCategory(books,category)
-
-result
-
-
-___________________________________
-6.	Change a book from Non-fiction to fiction, or vice versa.
-db.book.update({_id:1},{$set:{category:['Non-Fiction']}})
-_____________________________________
-
-
-
-
-
+);   
