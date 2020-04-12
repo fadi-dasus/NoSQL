1bb08b0efc76a8ac6122731c7abacf04062b018c
fadi-dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-04-13 00:23:27 +0200

    Refactoring the queries

--- script/AssignmentQuery.js
+++ script/AssignmentQuery.js
@@ -1,27 +1,10 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 1.all books by an author
 
-
-var authorId = db.author.find({name:'Bodil toft'},{_id:1}).next()._id
-
-var matchAuthor = {$match: {author:12}}
-        
-var unwindAuthorArray = {$unwind:{ path:'$author' , preserveNullAndEmptyArrays:true}}
-    
-var projectBookAuthor={$project:{author:1,ISBN:1}}    
-
-var groupBooksByAuthor = { $group: { _id : '$author',books:{$addToSet:'$ISBN'}    }  }
-    
-    
-db.book.aggregate([unwindAuthorArray,matchAuthor,projectBookAuthor,groupBooksByAuthor])
-
-
-
+var authorId = db.author.find({name:'Bodil toft'},{_id:1}).next()._idvar matchAuthor = {$match: {author:authorId}}        db.book.aggregate([matchAuthor])///ordb.book.find( { author: { $eq: authorId } } )
 ______________-
 
-
-
-2. total price of an order
+2. total price of an orderdb.order.aggregate([    { "$unwind": "$book" },    { "$group": {  _id: '$_id' , totalAmount: { $sum: { $multiply: [ "$book.qty", "$book.price" ] } }    }}])
 
 var booksTotalPice=  {
         $addFields: {
@@ -43,11 +26,8 @@ var unwind={$unwind:{path:'$book'}}
 var groupByOrderId = {   $group:{ _id:'$_id', TotalSale:{$sum:'$book.total'} }}
 
 db.order.aggregate([booksTotalPice,unwind,groupByOrderId])
-
-
+
 //or 
-
-
 
 var map = function() {
                        for (var idx = 0; idx < this.book.length; idx++) {
@@ -58,8 +38,8 @@ var map = function() {
 											emit(key, total); 
                                        }
                     };
-                    
-                    
+                   
+        
 var reduce = function(keyId, ObjVals) {
                         var total = 0;
                      for (var idx = 0; idx < ObjVals.length; idx++) {
@@ -76,43 +56,14 @@ db.orderTotal.find()
 __________________________________
 
 3.total sale for a cutomer
-
-var cutomer= {    $match:{ name:{$eq:'Clause'}    }   }
-var unwindordersArray = {    $unwind:{path:'$orderNo' ,  preserveNullAndEmptyArrays:true  }   }
-var joinCustomerOrders = {   $lookup:{ from:'order', foreignField:'_id', localField:'orderNo',  as : 'Customer_Order'    }   }    
-var groupByCutomerId =  {    $group:{ _id:'$_id',  name:{$max:'$name'},   email:{$max:'$email'},   orderNo:{$addToSet:'$orderNo'},   orders:{$addToSet:'$Customer_Order'}       }}
-var unwindBooksObjectArray = {   $unwind:{path:'$Customer_Order.book' ,   preserveNullAndEmptyArrays:true  }    }
-        
-
-db.customer.aggregate([cutomer,unwindordersArray,joinCustomerOrders,unwindBooksObjectArray,groupByCutomerId])
+var cutomer= {    $match:{ name:{$eq:'Clause'}    }   }var joinCustomerOrders = {   $lookup:{ from:'order', foreignField:'_id', localField:'orderNo',  as : 'Customer_Order'    }   }    var unwindCustomer_Order = { "$unwind": "$Customer_Order" }var unwindCustomerOrderBooks = { "$unwind": "$Customer_Order.book" }var groupByCustomerName = { $group: {  _id: "$name" , totalAmount: { $sum: { $multiply: [ "$Customer_Order.book.qty", "$Customer_Order.book.price" ] } }}}db.customer.aggregate([cutomer,joinCustomerOrders,unwindCustomer_Order,unwindCustomerOrderBooks,groupByCustomerName])
 _____________________________________________
 
 4.	Books that are categorized as neither fiction nor non-fiction
-
-var unwindedCatgoryArrayBefore= {$unwind:{path:'$category', preserveNullAndEmptyArrays:true} }
-
-var unwindedCatgoryArrayAfter = {$unwind:{path:'$book_category', preserveNullAndEmptyArrays:true} }
-
-var lookupBookCataegory = { $lookup:{from:'category', foreignField:'name',localField:'category',as : 'book_category'  }};
-
-var matchBooksCategory = {$match:{ 'book_category.name':{$nin:['Non-Fiction','Fiction']}}}
-
-db.book.aggregate([unwindedCatgoryArrayBefore,lookupBookCataegory,unwindedCatgoryArrayAfter,matchBooksCategory])
-
-
-__________________________________
+db.book.find( { category: { $nin: [ "Fiction", "Non-Fiction" ] } } )_______________________
 5.	Average page count by genre
 
-var unwindedCatgoryArrayBefore= {$unwind:{path:'$category', preserveNullAndEmptyArrays:true} }
-
-var unwindedCatgoryArrayAfter = {$unwind:{path:'$book_category', preserveNullAndEmptyArrays:true} }
-
-var lookupBookCataegory = { $lookup:{from:'category', foreignField:'name',localField:'category',as : 'book_category'  }};
-
-var groupByAveragePagesByGenre = {$group:{_id:'$book_category.name', 'average pages':{$avg:'$pages'}}}
-
-db.book.aggregate([unwindedCatgoryArrayBefore,lookupBookCataegory,unwindedCatgoryArrayAfter,groupByAveragePagesByGenre])
-
+db.book.aggregate( [  {  $group:  {  _id: "$category", avgPages: { $avg: "$pages" }     }   } ])
 __________________________________
 
 6.	Categories that have no sub-categories
@@ -123,7 +74,7 @@ db.category.find({name:{$nin:parents}})
 ___________
 
 7.ISBN numbers of books with more than one author
-db.book.find({author:{$size:2 }}).count()  
+db.book.find({author:{$not:{$size:1 }}}).count()
 		
 		
 __________________________________
@@ -140,7 +91,7 @@ db.order.aggregate([unwindBooksArray,groupByQty,matchSallingNumber])
 __________________________________
 
 
-9.Number of copies of each book sold – unsold books should show as 0 sold copies.see page 112 find all books and subtract the array of isbns and then project every thing var unwindBooksArray = {$unwind:'$book'}var joinBookOrder = { $lookup:{from:'book', foreignField:'ISBN',localField:'book.ISBN',as : 'booksDetails'  }};var unwindBooksDetailsArray = {$unwind:'$booksDetails'}var groupByISBN = {$group:{ _id:'$book.ISBN',title:{$max:'$booksDetails.title'},quantity_Sold :{$sum:'$book.qty'}}}var booksSaling = db.order.aggregate([unwindBooksArray,joinBookOrder,unwindBooksDetailsArray,groupByISBN]).toArray()var soldBooks = booksSaling.map( x => x._id ).sort()var unsoldBooks = db.book.aggregate([{$match:{ISBN:{$nin:soldBooks}}},{$project:{ ISBN:1,title:1,quantity_Sold:{$literal:0}}}]).toArray()       db.order.aggregate([     { $project: { books: { $setUnion: [ unsoldBooks, booksSaling] },_id:0}},{$unwind:'$books'},     {$group:{_id:'$books._id',title:{$max:'$books.title'},quantity_Sold :{$max:'$books.quantity_Sold'}}}  ])                            
+9.Number of copies of each book sold – unsold books should show as 0 sold copies.var unwindBooksArray = {$unwind:'$book'}var joinBookOrder = { $lookup:{from:'book', foreignField:'ISBN',localField:'book.ISBN',as : 'booksDetails'  }};var unwindBooksDetailsArray = {$unwind:'$booksDetails'}var groupByISBN = {$group:{ _id:'$book.ISBN',title:{$max:'$booksDetails.title'},quantity_Sold :{$sum:'$book.qty'}}}var booksSaling = db.order.aggregate([unwindBooksArray,joinBookOrder,unwindBooksDetailsArray,groupByISBN]).toArray()var soldBooks = booksSaling.map( x => x._id )var unsoldBooks = db.book.aggregate([{$match:{ISBN:{$nin:soldBooks}}},{$project:{ ISBN:1,title:1,quantity_Sold:{$literal:0}}}]).toArray()       db.order.aggregate([     { $project: { books: { $setUnion: [ unsoldBooks, booksSaling] },_id:0}},{$unwind:'$books'},     {$group:{_id:'$books._id',title:{$max:'$books.title'},quantity_Sold :{$max:'$books.quantity_Sold'}}}  ])                            
 
 ______________-
 
@@ -165,29 +116,7 @@ ______
 
 11.	Best-selling genres: The top 3 selling genres ordered in descending order by number of sales.
 
-
-
-var unwindBooksArray = {$unwind:'$book'}
-
-var rightJoinBookOrder = { $lookup:{from:'book', foreignField:'ISBN',localField:'book.ISBN',as : 'booksDetails'  }};
-
-var unwindBooksDetailsArray = {$unwind:'$booksDetails'}
-
-var groupByISBN = {$group:{ _id:'$book.ISBN',title:{$max:'$booksDetails.title'},category:{$max:'$booksDetails.category'},quantity_Sold :{$sum:'$book.qty'}}}
-
-var unwindCategoryArray = {$unwind:'$category'}
-
-var sortByTopSallingGenre={$sort:{'salling':-1}}
-
-var joinCategory={$lookup:{from:'category',foreignField:'name',localField:'category',as:'category_name'}}
-
-var unwindCategoryName = {$unwind:'$category_name'}
-
-var groupByCategoryName= {$group:{_id:'$category_name.name',salling:{$sum:'$quantity_Sold'}}}
-
-
-db.order.aggregate([unwindBooksArray,rightJoinBookOrder,unwindBooksDetailsArray,groupByISBN,
-unwindCategoryArray,joinCategory,unwindCategoryName,groupByCategoryName,sortByTopSallingGenre])
+var unwindBooksArray = {$unwind:'$book'}var rightJoinBookOrder = { $lookup:{from:'book', foreignField:'ISBN',localField:'book.ISBN',as : 'booksDetails'  }};var unwindBooksDetailsArray = {$unwind:'$booksDetails'}var groupByISBN = {$group:{ _id:'$book.ISBN',title:{$max:'$booksDetails.title'},category:{$max:'$booksDetails.category'},quantity_Sold :{$sum:'$book.qty'}}}var groupByCategoryName= {$group:{_id:'$category',salling:{$sum:'$quantity_Sold'}}}var sortByTopSallingGenre={$sort:{'salling':-1}}db.order.aggregate([unwindBooksArray,rightJoinBookOrder,unwindBooksDetailsArray,groupByISBN,groupByCategoryName,sortByTopSallingGenre,{$limit:3}])
 
 
 ____________________
